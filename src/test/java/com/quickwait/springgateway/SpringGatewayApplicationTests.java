8f3aae555dfd4a4293ff669ad8e40121009f4d20
Daniel Dutra <danieldhsd@gmail.com>
2022-02-22 09:01:32 -0300

    Creating gateway project

---
+++ src/test/java/com/quickwait/springgateway/SpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickwait.springgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
