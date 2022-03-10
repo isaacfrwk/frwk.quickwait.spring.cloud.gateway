5a8436269c71730bd64576da206f7b7ca2f7d467
Daniel Dutra <danieldhsd@gmail.com>
2022-03-10 09:48:11 -0300

    Revert "Update SpringGatewayApplication.java"
    This reverts commit b60f09b41f87fd0428565eb59044535a5e16a5e2.

--- src/main/java/com/quickwait/springgateway/SpringGatewayApplication.java
+++ src/main/java/com/quickwait/springgateway/SpringGatewayApplication.java
@@ -34,15 +34,5 @@ public class SpringGatewayApplication {
 		});
 		return groups;
 	}
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
-	}
 
 }
